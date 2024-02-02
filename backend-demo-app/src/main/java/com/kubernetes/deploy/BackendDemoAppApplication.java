a710e63ba253a57d58dc32bab5cf2b3a46ca9904
zees007 <zees007@rediffmail.com>
2024-02-02 21:10:33 +0530

    first commit

---
+++ backend-demo-app/src/main/java/com/kubernetes/deploy/BackendDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDemoAppApplication.class, args);
+    }
+
+}
---
+++ backend-demo-app/src/test/java/com/kubernetes/deploy/BackendDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetes.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
